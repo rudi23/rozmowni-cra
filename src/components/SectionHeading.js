05c55f9612d2a8eb406f2742ddc0e22abfa59216
Krzysztof Rudowski <krzysztof.rudowski@schibsted.pl>
2021-08-11 00:59:59 +0200

    tweak section

--- src/components/SectionHeading.js
+++ src/components/SectionHeading.js
@@ -3,7 +3,7 @@ import styles from './SectionHeading.module.scss'
 export default function SectionHeading({ heading, subheading }) {
     return (
         <div className={styles.root}>
-            <span className={styles.subheading}>{subheading}</span>
+            {subheading && <span className={styles.subheading}>{subheading}</span>}
             <h2 className={styles.heading}>{heading}</h2>
         </div>
     )
