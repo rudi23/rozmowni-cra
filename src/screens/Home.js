dbf8a02d666175b961e2cb750ffaf57c230e4fa5
Krzysztof Rudowski <krzysztof.rudowski@schibsted.pl>
2021-08-18 00:48:07 +0200

    refactor

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,7 @@
-import Banner from '../Banner';
-import Features from '../Features';
-import Idea from '../Idea';
-import Conversations from '../Conversations';
+import Banner from '../components/Banner';
+import Features from '../components/Features';
+import Idea from '../components/Idea';
+import Conversations from '../components/Conversations';
 
 export default function Home() {
     return (
